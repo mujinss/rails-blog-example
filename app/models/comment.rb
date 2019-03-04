871d4e18345b7d62b51e26e180b6c463dc9cc63b
ss <helensunlife@gmail.com>
2019-03-03 21:55:29 -0800

    added User

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  post_id    :integer
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ApplicationRecord
   belongs_to :post
+  belongs_to :user
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  belongs_to :user
 end
